d1eef8000dd14765852965b8f5102e8413d6633e
fstellmann <f.stellmann93@gmx.net>
2018-12-06 14:06:39 +0100

    Implementing more Tests

---
+++ UnitTestProject/FormTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MTGCardChecker;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class FormTest
+    {
+        [TestMethod]
+        public void testStatisticForm()
+        {
+
+        }
+    }
+}
